dc79a6875f17c3b267a84710eaff46ed63371d73
Markus Tschlatsche <markus.tschlatscher@gmail.com>
2015-12-25 20:09:22 +0100

    printIndu() Zugefügt

--- src/nQueens/include/individual.h
+++ src/nQueens/include/individual.h
@@ -9,20 +9,27 @@ namespace nQueens{
 class individual
 {
     public:
-        static unsigned int score(std::vector<unsigned int> & v);                // calculates the score of the individual
-        individual(unsigned int n); // constructor
+        //konstruktor
+        individual(unsigned int n);
         individual(unsigned int n, std::vector<unsigned int> & v);
-        virtual ~individual();      // destructor
-        unsigned int getScore();    // returns the score of the individual
-        bool isLocked();
-        std::vector<unsigned int> & getVector(); // returns reference to the position vector
-    protected:
-    private:
-        std::vector<unsigned int> v; // vector holding the horizontal positions of the queens
-        unsigned int n;              // constant number of rows
-        bool locked;                 // flag indicating this individual is used in a variation operator
-        unsigned int sc;          // score of the individual
-        void init();                 // initialize class variables
+
+        //destruktor
+        virtual ~individual();
+
+        //Methoden
+        static unsigned int score(std::vector<unsigned int> & v);   // calculates the score of the individual
+        unsigned int getScore();                                    // returns the score of the individual
+        bool isLocked();                                            // returns if Flag is true / false
+        std::vector<unsigned int> & getVector();                    // returns reference to the position vector
+        void printIndu();                                           // gibt alle Werte eines Indi aus (später nur den Vektor);
+    //protected:
+    //private:
+        //Members
+        std::vector<unsigned int> v;    // vector holding the horizontal positions of the queens
+        unsigned int n;                 // constant number of rows
+        bool locked;                    // flag indicating this individual is used in a variation operator
+        unsigned int sc;                // score of the individual
+        void init();                    // initialize class variables
 };
 }
 #endif // INDIVIDUAL_H
