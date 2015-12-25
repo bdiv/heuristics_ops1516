4cabe41dd9a9686e2a24fbf5bcae594efc3bccad
Markus Tschlatsche <markus.tschlatscher@gmail.com>
2015-12-26 00:34:35 +0100

    Auch für die Alternative

--- src/nQueens/main.cpp
+++ src/nQueens/main.cpp
@@ -1,20 +1,26 @@
 #include<iostream>
 #include"population.h"
+#include<stdlib.h>
+#include<time.h>
 
 using namespace std;
 
 int main()
 {
+
     //Festlegung der Parameter später als Benutzereingabe
     unsigned int    n = 5;      //Größe des Schachbretts
-    unsigned int    x = 4;      //Wieviele Individuen pro Generation (Errechnen oder über Eingabe ?)
+    unsigned int    x = 1;      //Wieviele Individuen pro Generation (Errechnen oder über Eingabe ?)
     unsigned int    i = 4;      //Maximale Anzahl der Iterationen
     double p = 0.2;             //Mutationswahscheinlichkeit
+    srand(time(NULL));
 
     //erstellung der ersten Generation  mit population/void init()
     //2 Möglichkeiten:
+
     //1: Mit komplett zufälligen Individuen
     nQueens::population poli(n,x,p);
+
     //2: Da wollten wir doch was machen das es keine horizontalen überschneidungen gibt ?
     //oder wahr das gleich die erste und einzige Möglichkeit
 
