17c4fb2e7f55dbc8caa20347285baa6398b8a41c
Markus Tschlatsche <markus.tschlatscher@gmail.com>
2015-12-25 20:07:33 +0100

    PrintIndu() Methode Geschrieben

--- src/nQueens/src/individual.cpp
+++ src/nQueens/src/individual.cpp
@@ -1,6 +1,10 @@
-#include "individual.h"
+#include"individual.h"
+#include<iostream>
 
 namespace nQueens {
+
+//konstruktor nur mit Länge n
+//Bei der Init werden immer die gleichen hergestellt :-(  können wir das noch anders machen / oder ist das egal ?
 individual::individual(unsigned int n)
 {
     this->n = n;
@@ -9,6 +13,7 @@ individual::individual(unsigned int n)
     this->init();
 }
 
+//Konstruktor mit Länge n und einen Vektor (Für Paarung ?)
 individual::individual(unsigned int n, std::vector<unsigned int> & v)
 {
     this->n = n;
@@ -16,15 +21,31 @@ individual::individual(unsigned int n, std::vector<unsigned int> & v)
     this->v = v;
 }
 
+//destruktor
 individual::~individual()
 {
     //dtor
 }
 
+//Methoden
+void individual::printIndu()
+{
+    std::cout << "Position der Damen horizontal = ";
+    for(int i=0;i<n;i++)
+    {
+        std::cout << v[i] << ",";
+    }
+    std::cout << std::endl;
+    int score = getScore();
+    std::cout << "Score = " << score << std::endl;
+    //std::cout << "Flag =  " << locked << std::endl;
+}
+
 void individual::init()
 {
     this->locked = false;
-    individual::score(this->v);
+    //individual::score(this->v); // wusste nicht was das machen soll ?
+    this->sc = score(this->v);
 }
 
 unsigned int individual::getScore()
