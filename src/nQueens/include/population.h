e50e5980fce9b2fd56c609f6ce1c2df0dc71570e
Markus Tschlatsche <markus.tschlatscher@gmail.com>
2015-12-25 20:06:11 +0100

    print Indivdual hinzugefühgt

--- src/nQueens/include/population.h
+++ src/nQueens/include/population.h
@@ -7,16 +7,26 @@ namespace nQueens {
 class population
 {
     public:
-        population(unsigned int n, unsigned int x, double pMutation);               // constructor
-        void init();                     // initialize population with random individuals
-        virtual ~population();      // destructor
-        individual& getRandomIndividual(); // return a random individual which is not in use (indicated by flag)
-        std::pair<individual&,individual&> chooseTwoRandom(); // returns a pair of random parent individuals, first will have the lower score
-    protected:
+
+        //konstruktor
+        population(unsigned int n, unsigned int x, double pMutation);
+
+        //destruktor
+        virtual ~population();
+
+        //methoden
+        void init();        // initialize population with random individuals
+        //individual& getRandomIndividual();                      // return a random individual which is not in use (indicated by flag)
+        //dstd::pair<individual&,individual&> chooseTwoRandom();   // returns a pair of random parent individuals, first will have the lower score
+        //void scoreAll();    //Alle Individuen bekommen einen score;
+        void printAll();    //gibt alle indu aus (Test methode)
+
     private:
-        double pMutation;      // probability for mutation after cross
-        unsigned int n;                 // number of genes an individual has
-        std::vector<individual> individuals; // datastructure for the individuals
+
+        //Members
+        double pMutation;                       // probability for mutation after cross
+        unsigned int n;                         // number of genes an individual has
+        std::vector<individual> individuals;    // datastructure for the individuals
 };
 }
 #endif // POPULATION_H
