9fbf36fec17fac8fac17700ee01c5f11e87572e1
bdiv <mzxlii@gmx.de>
2015-12-28 21:49:32 +0100

    added good and funny commentary

--- src/nQueens/include/iterativeMaster.h
+++ src/nQueens/include/iterativeMaster.h
@@ -8,17 +8,27 @@ namespace nQueens {
 class iterativeMaster
 {
     public:
+        // constructor
         iterativeMaster( unsigned int n, unsigned int x, double pMutation );
+        //destructor
         virtual ~iterativeMaster();
+        // getter for the population
         population& getPopulation();
+        // getter for current number of variations during runtime
         unsigned int getVariationsCounter();
+        // getter for solutions
         std::vector<individual> & getSolutions();
+        // solver parameterized by variations
         unsigned int solve(unsigned int maxVariations, crossbreedFunctor & breed);
+        // solver that runs until it reached a specified point in time (timeout)
         unsigned int solve(std::time_t timeout, crossbreedFunctor & breed);
     protected:
     private:
+        // containes the individuals
         population pop;
+        // contains the number of variations during runtime/solving
         unsigned int variationCounter;
+        // contains the solutions (score == 0)
         std::vector <individual> solutions;
 };
 
--- src/nQueens/src/iterativeMaster.cpp
+++ src/nQueens/src/iterativeMaster.cpp
@@ -3,6 +3,7 @@
 
 namespace nQueens {
 
+// simple constructor
 iterativeMaster::iterativeMaster(unsigned int n, unsigned int x, double pMutation): pop(population(n,x,pMutation))
 {
 
@@ -13,46 +14,76 @@ iterativeMaster::~iterativeMaster()
     //dtor
 }
 
+// return the population as a reference
 population& iterativeMaster::getPopulation()
 {
     return this->pop;
 }
 
+// return the number of variations that occured during runtime
 unsigned int iterativeMaster::getVariationsCounter()
 {
     return this->variationCounter;
 }
 
+// return vector containing the solutions
 std::vector <individual> & iterativeMaster::getSolutions()
 {
     return this->solutions;
 }
 
+// solve the optimization problem using a special functor
+// the functor needs to implement the crossbreedFunctor class
+// and overload the ()operator!
+// this runs until we've reached a specified number of variations
+// returns the numbe rof solutions found
+// those can be recovered by calling getSolutions()
 unsigned int iterativeMaster::solve(unsigned int maxVariations, crossbreedFunctor& breed)
 {
+    // loop until we reach maximum variation count
     for(unsigned int i = 0; i < maxVariations; i++)
     {
+        // use the crossbreedFunctor to create a new individual
+        // he'll automatically be in the population and replace the parent with
+        // the lowest score
         individual dude = breed(this->pop);
+        // if score is 0 we have found a solution, scores > 0 are suboptimal
         if(dude.getScore() == 0)
         {
+            // grab and sack the dude
             this->solutions.push_back(dude);
         }
     }
+    // return number of solutions
     return this->solutions.size();
 }
 
+// solves our optimizationproblem using a special crossbreedFunctor
+// this will run until we reach the specified timeout
+// (note that timeout is not a range but rather a future point in time!!)
+// and returns the number of solutions found
+// those can later be recovered by calling getSolutions()
 unsigned int iterativeMaster::solve(std::time_t timeout, crossbreedFunctor & breed)
 {
+    // save current time
     std::time_t now;
     std::time(&now);
+    // check if we've run past the timeout
     while(now < timeout)
     {
+        // breed a dude
+        // he'll automatically be in the population and replace the parent with
+        // the lowest score
         individual dude = breed(this->pop);
+        // check the dude
+        // scores equal 0 are solutions, scores > 0 are suboptimal
         if(dude.getScore() == 0)
         {
+            // grab'n'sack the dude
             this->solutions.push_back(dude);
         }
     }
+    // return number of solutions found
     return this->solutions.size();
 }
 
