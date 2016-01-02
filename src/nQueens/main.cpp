86b34a4663df68793cdc443af03eb2b87401d9c0
Markus Tschlatsche <markus.tschlatscher@gmail.com>
2016-01-02 18:52:54 +0100

    New Main

--- src/nQueens/main.cpp
+++ src/nQueens/main.cpp
@@ -1,36 +1,57 @@
 #include<iostream>
 #include"include/population.h"
 #include<stdlib.h>
+#include<sstream>
 #include<time.h>
-
-using namespace std;
 
 int main()
 {
+    //Init of Parameters
+    unsigned int    n=0;      //Size of Chessboard;
+    unsigned int    x=1;      //How many Individuals per Generation;
+    double p = 0.2;           //Probability of mutation;
+    //unsigned int    i=4;      //Max Iteration;
 
-    //Festlegung der Parameter später als Benutzereingabe
-    unsigned int    n = 5;      //Größe des Schachbretts
-    unsigned int    x = 1;      //Wieviele Individuen pro Generation (Errechnen oder über Eingabe ?)
-    unsigned int    i = 4;      //Maximale Anzahl der Iterationen
-    double p = 0.2;             //Mutationswahscheinlichkeit
-    srand(time(NULL));
-
-    //erstellung der ersten Generation  mit population/void init()
-    //2 Möglichkeiten:
-
-    //1: Mit komplett zufälligen Individuen
-    nQueens::population poli(n,x,p);
-
-    //2: Da wollten wir doch was machen das es keine horizontalen überschneidungen gibt ?
-    //oder wahr das gleich die erste und einzige Möglichkeit
-
-    // horizontale überschneidungen gibt es nicht, wenn die population erstellt wird.
-    // Sie könnte theoretisch auftreten, wenn man einen blöden crossbreedFunctor designed.
-    // die jetzigen sollten aber darauf achten
-    // als letztes Notfallnetz haben wir immer die Score, die unterirdisch schlecht wird,
-    // wenn wir zusätzlich zu diagonalen auch horizontale Überschneidungen haben
-
-    poli.printAll();
+    std::string input = "";
+    //input n;
+    while(true)
+    {
+        std::cout << "Size of Chessboard (min 4) ";
+        getline(std::cin,input);
+        std::stringstream myStream(input);
+        if(myStream >> n)
+        {
+            if(n>3)
+            break;
+        }
+        std::cout << "Invalid" << std::endl;
+    }
+    //input x;
+    while(true)
+    {
+        std::cout << "Number of Individuals " << std::endl;
+        std::cout << "(Tipp: Small n big Number) = ";
+        getline(std::cin,input);
+        std::stringstream myStream(input);
+        if(myStream >> x)
+            break;
+        std::cout << "Invalid" << std::endl;
+    }
+    //input p;
+    while(true)
+    {
+        std::cout << "Probability for Mutation " << std::endl;
+        std::cout << "(Tipp: Standard is 0.1) = ";
+        getline(std::cin,input);
+        std::stringstream myStream(input);
+        if(myStream >> p)
+            break;
+        std::cout << "Invalid" << std::endl;
+    }
+    //summary of Inputs
+    std::cout << "Size = " << n << std::endl;
+    std::cout << "Induviduen = " << x << std::endl;
+    std::cout << "Mutation = " << p << std::endl;
 
     return 0;
 }
